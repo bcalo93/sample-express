e2e909c6ce51d6889d249b4375a60ab2b2937ad8
Brahian Calo <bcalo93@gmail.com>
2021-05-10 21:09:11 -0300

    Added route as middleware. Open issue fix modules resolution or make resolver actually work

--- src/controllers/routes.ts
+++ src/controllers/routes.ts
@@ -5,3 +5,5 @@ const router = Router()
 const controller = new PetController()
 
 router.get('/pets', (req, res, next) => controller.findAll(req, res, next))
+
+export { router }
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import express from 'express'
+import { router } from './controllers'
+
 const app = express()
 const port = 8080
 
 export default () => {
-    app.get('/', (req, res) => {
-        res.send('Hello World!')
-    })
+    app.use(router)
 
     app.listen(port, () => {
         console.log(`server started at http://localhost:${port}`)
